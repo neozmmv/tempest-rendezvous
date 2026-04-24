56b414d1b84390dd00b37b8babe637f6081c9fce
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-04-24 15:43:35 -0300

    added cors

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@ mod routes;
 
 use axum::{Router, routing::get, routing::post};
 
+use tower_http::cors::{Any, CorsLayer};
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().nest("/api", routes::router());
+    // CORS
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    let app = Router::new().nest("/api", routes::router()).layer(cors);
     let port = 3000;
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
